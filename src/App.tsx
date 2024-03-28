83592b125a335e1691c0d83b6f20af54fa72572f
Chilaka Michael Obinna <obichilaka@gmailcom>
2024-03-28 19:01:46 +0100

    updates on App.tsx & index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="site">
+      <Navbar />
+<main>
+  
+</main>
+      <Footer />
+    </div>
   )
 }
 
