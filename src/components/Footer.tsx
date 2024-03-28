c7b29cc02f5e7bbfbdd429582a9cd1bfbe855239
Chilaka Michael Obinna <obichilaka@gmailcom>
2024-03-28 18:50:33 +0100

    Footer.tsx & App.tsx updated

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 p-4 mt-8">
+      <div className="max-w-7xl mx-auto text-white text-center">
+        <p>Stock App &copy; 2024. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
